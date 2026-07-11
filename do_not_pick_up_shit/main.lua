16aec7ce1b0d4a748e108f28067fcfe41547b4ee
changchanggod <241880626@smail.nju.edu.cn>
2026-07-11 13:47:48 +0800

    feat(DNPUS):初始化了挑战“不要搬史”的文件框架

---
+++ do_not_pick_up_shit/main.lua
@@ -0,0 +1,17 @@
+if CCGChallenges45768==nil then
+    CCGChallenges45768 =RegisterMod("don't pick up shit", 1)
+    local mod= CCGChallenges45768
+    local json=require("json")
+    if mod:HasData() then
+        local data=mod:LoadData()
+        mod.Data=json.decode(data)
+    end
+    function mod:saveMyData()
+        if mod.Data then
+           mod:SaveData(json.encode(mod.Data)) 
+        end
+    end
+    mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.saveMyData)
+end
+CCGChallenges45768 =RegisterMod("don't pick up shit", 1) --only in Debug  TODO remove
+include("script.DNPUS.do_not_pick_up_shit")
