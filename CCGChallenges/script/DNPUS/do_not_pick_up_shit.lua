feb408514b6a24fe11c57be47b5f711e027dd474
changchanggod <241880626@smail.nju.edu.cn>
2026-07-11 15:14:50 +0800

    feat(DNPUS):初步实现了生成屎的逻辑，道具添加、显示正常

--- CCGChallenges/script/DNPUS/do_not_pick_up_shit.lua
+++ CCGChallenges/script/DNPUS/do_not_pick_up_shit.lua
@@ -1,18 +1,51 @@
 local mod= CCGChallenges45768
-
-local function GetChallengeSafe(challengeName)
-    local cId = Isaac.GetChallengeIdByName(challengeName)
-    if cId == -1 then
-        Isaac.ConsoleOutput(string.format("[CCG][Error]: Challenge \"%s\" load failed, missing challenge\n", challengeName))
-        return nil
-    end
-    return cId
+local utils=require("script.CCG_utils")
+---@type integer|nil
+local challengeId=utils.GetChallengeSafe("don't pick up shit")
+---@type integer|nil
+local shitId=utils.GetItemSafe("bad shit")
+if challengeId==nil or shitId==nil then
+    return nil
 end
 
-local challengeId=GetChallengeSafe()
+local setting=nil
+local resetting=false
+local function reset()
+    if type(mod.Data)~=table then
+        mod.Data={}
+    end
+    mod.Data.DNPUSSetting={
+        shitNum=1,
+        autoIncrease=1,
+        bigRoomSpecial=true,
+        avoidDoorKill=true
+    }
+    setting=mod.Data.DNPUSSetting
+end
+if mod.Data and mod.Data.DNPUSSetting then
+    setting=mod.Data.DNPUSSetting
+else
+    reset()
+end
 
+utils.allowChallengeSecretPath(challengeId,false)
 local function spawnShit(num)
-    for i = 1, 10, 1 do
-        
+    local game=Game()
+    local room=game:GetRoom()
+    for i = 1, num, 1 do
+        local pos=room:GetRandomPosition(1)
+        game:Spawn(5,100,pos,Vector(0, 0),nil,shitId,room:GetSpawnSeed())
     end
 end
+local function perRoomSpawnShit(_)
+    local game=Game()
+    local room=game:GetRoom()
+    if room:IsClear() then
+        return
+    end
+    if not setting then
+        return
+    end
+    spawnShit(setting.shitNum)
+end
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM,perRoomSpawnShit)
