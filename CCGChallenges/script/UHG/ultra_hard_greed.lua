2cecffce23e2c5ce93efb2b5725970ef5f08e434
changchanggod <241880626@smail.nju.edu.cn>
2026-07-16 22:13:17 +0800

    fix(UHG):优化了精英怪的生成逻辑

--- CCGChallenges/script/UHG/ultra_hard_greed.lua
+++ CCGChallenges/script/UHG/ultra_hard_greed.lua
@@ -5,12 +5,61 @@ local challengeId = utils.GetChallengeSafe(challengeName)
 if challengeId == nil then
     return nil
 end
+local myRNG=utils.getRNG()
+
+local championWeight={
+    [0]=20,
+    [1]=20,
+    [2]=20,
+    [3]=10,
+    [4]=10,
+    [5]=20,
+    [6]=1,
+    [7]=20,
+    [8]=2,
+    [9]=2,
+    [10]=20,
+    [11]=2,
+    [12]=10,
+    [13]=20,
+    [14]=5,
+    [15]=5,
+    [16]=5,
+    [17]=5,
+    [18]=5,
+    [19]=5,
+    [20]=2,
+    [21]=2,
+    [22]=2,
+    [23]=2,
+    [24]=2,
+    [25]=1,
+}
+local prefix = {}
+local total = 0
+for key, v in pairs(championWeight) do
+    total = total + v
+    table.insert(prefix, {sum = total, data = key})
+end
+
 local championIgnore={}
 ---@param entN EntityNPC
 local function turnIntoChampion(_,entN)
-    if entN:IsChampion() or championIgnore[utils.TypeToNum(entN.Type,entN.Variant,entN.SubType)] then
-        return
+    if Game().Challenge==challengeId then
+        if entN.SpawnerEntity then
+            print(entN.Type,entN.Variant,entN.SubType)
+        end
+        if entN:IsBoss() or entN:IsChampion() or championIgnore[utils.TypeToNum(entN.Type,entN.Variant,entN.SubType)] then
+            return
+        end
+        local rand=myRNG:RandomInt(total)+1
+        for _, p in ipairs(prefix) do
+            if rand <= p.sum then
+                entN:MakeChampion(Game():GetSeeds():GetStartSeed(),p.data,true)
+                entN.HitPoints=entN.MaxHitPoints
+                break
+            end
+        end
     end
-    entN:MakeChampion(Game():GetSeeds():GetStartSeed(),-1,true)
 end
 mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT,turnIntoChampion)
